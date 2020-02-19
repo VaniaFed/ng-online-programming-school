66782a77d196ebdb3b1da13d992aed1739591889
Ivan_Fediakov <Ivan_Fediakov@epam.com>
2020-02-19 19:07:24 +0300

    feat: add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatTableModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
+
+import {TileGalleryComponent} from './tile-gallery/tile.gallery.component';
+import {TileComponent} from './tile/tile.component';
+
+const components = [
+  TileGalleryComponent,
+  TileComponent,
+];
+
+const modules = [
+  CommonModule,
+  RouterModule,
+  FormsModule,
+  ReactiveFormsModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatDialogModule,
+  MatTableModule,
+];
+
+@NgModule({
+  declarations: [
+    ...components
+  ],
+  imports: [
+    ...modules
+  ],
+  exports: [
+    ...components,
+    ...modules
+  ]
+})
+export class SharedModule { }
